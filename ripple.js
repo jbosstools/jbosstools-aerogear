f0503c5081e55414e8b80eab92c11e947f613cc5
Ilya Buziuk <ibuziuk@exadel.com>
2013-03-26 12:47:34 +0300

    Fixing camera API for correct collaboration with UploadFileServlet and HostFileServlet, moving to jQuery ui dialog

--- ripple.js
+++ ripple.js
@@ -28133,11 +28133,10 @@ ripple.define('ui/plugins/camera', function (ripple, exports, module) {
 var ui = ripple('ui'),
     event = ripple('event'),
     upload = document.getElementById('file-upload'),
-    select = document.getElementById('select-file'),
     take = document.getElementById('take-file'),
     optOut = document.getElementById('camera-cancel'),
-    result = document.getElementById("camera-result");
-
+    result = document.getElementById('camera-result'),
+    uploadButton = document.getElementById('upload-file-button');
 function getType() {
     return (upload.getAttribute('accept') || "").replace("/*", "");
 }
@@ -28150,40 +28149,67 @@ function clear() {
 
 module.exports = {
     initialize: function () {
-        select.addEventListener('click', function () {
-            upload.click();
-        });
+       $("#camera-window").dialog({
+            autoOpen: false,
+            modal: true,
+            title: "Take Photo",
+            heigh: 50,
+            width: 400,
+            position: 'center',
+        }).hide();
+        
+        var wrapper = $('<div/>').css({height:0,width:0,'overflow':'hidden'}); //XXX adding wrapper for hiding <input type="file"/> (#file-upload)
+        var fileInput = $(upload).wrap(wrapper);
+        $(uploadButton).click(function() {
+          upload.click();
+        }).show();
 
-        upload.addEventListener('change', function () {
-            clear();
+        upload.addEventListener('change', function() {
+          var imageLink;
+          var iframe = $('<iframe name="postiframe" id="postiframe" style="display: none" />');//XXX adding iframe in order to get callback from UploadFileServlet (standart workaround)
+          $("body").append(iframe);
 
-            var capture = document.createElement(getType());
-            capture.setAttribute("controls");
-            capture.style.display = "inline";
-            capture.style.height = (screen.availHeight - 50) + "px";
-            capture.src = window.webkitURL.createObjectURL(upload.files[0]);
+          var form = $('#photo-form');
+          
+          form.attr("action", "fileupload");
+          form.attr("method", "post");
+          form.attr("enctype", "multipart/form-data");
+          form.attr("encoding", "multipart/form-data");
+          form.attr("target", "postiframe");
+          form.attr("photo", $('#file-upload').val());
 
-            result.appendChild(capture);
-
-
-            take.style.display = "inline";
+          form.submit();
+          $("#postiframe").load(function () {
+              iframeContents = $("#postiframe")[0].contentWindow.document.body.innerHTML;
+              $("#textarea").html(iframeContents);
+              response = $("#textarea").text();
+              imageLink= JSON.parse(response).photoUrl;
+              clear();
+              var capture = document.createElement(getType());
+              capture.style.height = "272px";
+              capture.style.display = "block";
+              capture.src = imageLink; 
+              result.appendChild(capture);
+              take.style.display = "inline";
+          });
+          return false;
         });
 
         take.addEventListener('click', function () {
             //console.log("type", getType());
             event.trigger('captured-' + getType(), [result.firstChild.src, upload.files[0]]);
-            module.exports.hide();
+             $("#camera-window").dialog("close");
         });
 
         optOut.addEventListener('click', function () {
             console.log('capture-image cancelled');
             event.trigger('image-capture-cancelled');
-            ui.hideOverlay("camera-window");
+            $("#camera-window").dialog("close");
         });
     },
     show: function (type) {
         type = type || "image";
-        ui.showOverlay("camera-window");
+        $("#camera-window").dialog("open");
 
         if (getType() !== type) {
             clear();
@@ -28206,7 +28232,6 @@ module.exports = {
         event.trigger('camera-closed');
     }
 };
-
 });
 ripple.define('ui/plugins/layout', function (ripple, exports, module) {
 /*
@@ -32574,7 +32599,7 @@ var _self,
                 },
                 show: function (callback, showContainer) {
                     _show(_overlay);
-                    if (!showContainer) {
+                    if (!showContainer && _container) {
                         _hide(_container);
                     }
                     _trigger(callback);
