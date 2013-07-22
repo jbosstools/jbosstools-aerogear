b28e2109efe8c392b22c556daab88265d17d144e
Konstantin Marmalyukov <kmarmaliykov@exadel.com>
2013-07-22 17:52:44 +0300

    JBIDE-16165 CordovaSim does not warn users that it needs Safari to run
    fixed
    stacktrace appeared after exception wrapped

--- plugins/org.jboss.tools.vpe.cordovasim/src/org/jboss/tools/vpe/cordovasim/CordovaSimRunner.java
+++ plugins/org.jboss.tools.vpe.cordovasim/src/org/jboss/tools/vpe/cordovasim/CordovaSimRunner.java
@@ -17,6 +17,7 @@ import java.text.MessageFormat;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTError;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.OpenWindowListener;
 import org.eclipse.swt.browser.WindowEvent;
@@ -72,8 +73,16 @@ public class CordovaSimRunner {
 			final Shell shell = new Shell(display);
 			setShellAttributes(shell);
 			shell.setLayout(new FillLayout());
-			final Browser browser = new Browser(shell, SWT.WEBKIT);
-			browser.setUrl("http://localhost:" + port + "/" + cordovaSimArgs.getStartPage() + "?enableripple=true");
+			final Browser browser;
+			try {
+				browser = new Browser(shell, SWT.WEBKIT);
+				browser.setUrl("http://localhost:" + port + "/" + cordovaSimArgs.getStartPage() + "?enableripple=true");
+			} catch(SWTError e) {
+				e.printStackTrace();
+				ExceptionNotifier.showWebKitLoadError(new Shell(display), e, "CordovaSim");
+				display.dispose();
+				return;
+			}
 			shell.addListener(SWT.Close, new Listener() {
 				@Override
 				public void handleEvent(Event event) {
