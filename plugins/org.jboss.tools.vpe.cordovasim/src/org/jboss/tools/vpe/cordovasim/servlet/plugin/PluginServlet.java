49b4572b06ef004ff579b580d37f3056b981c764
Ilya Buziuk <ibuziuk@exadel.com>
2013-08-14 16:35:36 +0300

    JBIDE-15302 CordovaSim: Adding caching via Etag for PluginServlet.

--- plugins/org.jboss.tools.vpe.cordovasim/src/org/jboss/tools/vpe/cordovasim/servlet/plugin/PluginServlet.java
+++ plugins/org.jboss.tools.vpe.cordovasim/src/org/jboss/tools/vpe/cordovasim/servlet/plugin/PluginServlet.java
@@ -10,11 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.jboss.tools.vpe.cordovasim.plugin.model.FileIdMapper;
+import org.jboss.tools.vpe.cordovasim.servlet.util.ServletUtil;
 
 public class PluginServlet extends HttpServlet {
+	private static final String ETAG = "Etag";
+	private static final String IF_NONE_MATCH = "If-None-Match";
 	private static final String CORDOVA_DEFINE = "cordova.define(";
-	private static final String FUNCTION_BEGGINING =  "function(require, exports, module) {\n";
-	private static final String FUNCTION_END =  "});";
+	private static final String FUNCTION_BEGGINING = "function(require, exports, module) {\n";
+	private static final String FUNCTION_END = "});";
 	private String pluginDir;
 	
 	public PluginServlet(String pluginDir) {
@@ -25,19 +28,38 @@ public class PluginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String pathInfo = req.getPathInfo();
-		String uri = req.getRequestURI();
-		String pluginId = generatePluginId(uri);
-		if (pluginId != null) {
-			File file = new File(pluginDir + pathInfo);
-			if (file.exists()) {
-				String fileContent = new Scanner(file).useDelimiter("\\A").next();
-				String content = CORDOVA_DEFINE + "\"" + pluginId + "\"" + ", " + FUNCTION_BEGGINING + fileContent + FUNCTION_END;
-				resp.setStatus(HttpServletResponse.SC_OK);
-				resp.setContentType("application/javascript");
-				resp.getWriter().write(content);
+		File file = new File(pluginDir + pathInfo);
+
+		if (file.exists()) {
+			String ifNoneMatchValue = req.getHeader(IF_NONE_MATCH);
+			String eTag = ServletUtil.generateEtagForFile(file);
+
+			if ((ifNoneMatchValue != null) && (eTag.equals(ifNoneMatchValue))) {
+				resp.setHeader(ETAG, eTag);
+				resp.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+				return;
+			} else {
+				String uri = req.getRequestURI();
+				String pluginId = generatePluginId(uri);
+				if (pluginId == null) {
+					resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+					return;
+				} else {
+					String fileContent = new Scanner(file).useDelimiter("\\A").next();
+					String content = CORDOVA_DEFINE + "\"" + pluginId + "\"" + ", " + FUNCTION_BEGGINING + fileContent + FUNCTION_END;
+					
+					resp.setStatus(HttpServletResponse.SC_OK);
+					resp.setContentType("application/javascript");
+					resp.setHeader(ETAG, eTag);
+					resp.getWriter().write(content);
+				}
 			}
+		} else {
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return;
 		}
 	}
+
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
