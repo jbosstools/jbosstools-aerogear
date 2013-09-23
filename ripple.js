64cfc354ac3b1d9716de11dfd3434c502eb8e927
Ilya Buziuk <ibuziuk@exadel.com>
2013-09-23 16:53:19 +0300

    JBIDE-14506 CordovaSim: Implementing cordova plugin support. Moving BarcodeScanner plugin to separate .js file. SplashScreen and inAppBrowser now part of cordova 2.0.x API. Commenting cordova 3.0.0 support

--- ripple.js
+++ ripple.js
@@ -40205,152 +40205,7 @@ module.exports = {
 };
 
 });
-ripple.define('phonegap-plugin/BarcodeScanner', function (ripple, exports, module) {
 
-  var emulatorBridge = ripple('emulatorBridge');
-
-  module.exports = {
-    scan: function (success, error, args) {
-      var scanDialog = $('<div id="scanDialog" class="scanDialog">'),
-          fieldSet = $('<fieldset id="skanDialogFieldSet" class="skanDialogFieldSet">'),
-
-          formatDropDownLabel = $('<label for="formatDropDown">Format:</label>'),
-          formatDropDown = $('<select id="formatDropDown">'),
-          qrCodeFormat = $('<option value="QR_CODE">QR_CODE</option>'),
-          dataMatrixFormat = $('<option value="DATA_MATRIX">DATA_MATRIX</option>'),
-          upcEFormat = $('<option value="UPC_E">UPC_E</option>'),
-          upcAFormat = $('<option value="UPC_A">UPC_A</option>'),
-          ean8Format = $('<option value="EAN_8">EAN_8</option>'),
-          ean13Format = $('<option value="EAN_13">EAN_13</option>'),
-          code128Format = $('<option value="CODE_128">CODE_128</option>'),
-          code39Format = $('<option value="CODE_39">CODE_39</option>'),
-
-          scannedTextLabel = $('<label for="scannedText">Scanned Text:</label>'),
-          scannedText = $('<input type="text" id=scannedText>'),
-
-          okButton = $('<button id="barcodeSkan-ok" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only small-button"><span class="ui-button-text">Ok</span></button>'),
-          cancelButton = $('<button id="barcodeScan-cancel" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only small-button"><span class="ui-button-text">Cancel</span></button>');
-      
-      try {
-            scannedTextLabel.appendTo(fieldSet);
-            scannedText.appendTo(fieldSet);
-               
-            // inizializing format Drop Down
-            qrCodeFormat.appendTo(formatDropDown);
-            dataMatrixFormat.appendTo(formatDropDown);
-            upcEFormat.appendTo(formatDropDown);
-            upcAFormat.appendTo(formatDropDown);
-            ean8Format.appendTo(formatDropDown);
-            ean13Format.appendTo(formatDropDown);
-            code128Format.appendTo(formatDropDown);
-            code39Format.appendTo(formatDropDown);
-            
-            formatDropDownLabel.appendTo(fieldSet);    
-            formatDropDown.appendTo(fieldSet);
-
-            fieldSet.appendTo(scanDialog);
-
-            okButton.appendTo(scanDialog);
-            cancelButton.appendTo(scanDialog);
-
-            scanDialog.appendTo(window.document.body);
-            
-            document.getElementById('barcodeSkan-ok').addEventListener("click", function() {
-                var result = new Object();
-                result.cancelled = false; 
-                result.text = $('#scannedText').val();
-                result.format = $('#formatDropDown option:selected').text();
-                $("#scanDialog").dialog('close');
-                success(result);
-            });
-
-           document.getElementById('barcodeScan-cancel').addEventListener("click", function() {
-                var result = new Object();
-                result.cancelled = true; 
-                result.text = "";
-                result.format = ""; 
-                $("#scanDialog").dialog('close');
-                success(result);
-            });
-
-            $("#scanDialog").dialog({
-                  autoOpen: true,
-                  modal: true,
-                  title: "Barcode Scanner",
-                  heigh: 50,
-                  width: 510,
-                  position: 'center',
-                  resizable: false
-              }).bind('dialogclose', function() {
-                 $(this).dialog('destroy').remove(); // Destroying dialog on close
-              });
-
-        } catch(err) {
-          error("Fail to emulate BarcodeScanner.scan() method");
-        }
-    },
-
-    encode: function (success, error, args) {
-       var data = args[0].data,
-           type = args[0].type,
-           url = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chld=L|5&chl=" + data, // Using google QR service - https://developers.google.com/chart/infographics/docs/qr_codes
-           backgroundMask = $('<div id="background-mask">'),
-           barcodeWrapper = $('<div id="barcode-wrapper">'),
-           barcodeImage = $('<img id="barcode-image">'),
-           barcodeData = $('<div id="barcode-data">' + data + '</div>');  
-           barcodeBackButtonFunction = function() {
-              var barcodeWrapper = emulatorBridge.window().document.getElementById('barcode-wrapper');
-              var backgroundMask = emulatorBridge.window().document.getElementById('background-mask');
-              
-              barcodeWrapper.parentNode.removeChild(barcodeWrapper);
-              backgroundMask.parentNode.removeChild(backgroundMask);
-              
-              emulatorBridge.window().document.removeEventListener("backbutton", barcodeBackButtonFunction, false); // Force one time execution of the listener
-           };
-
-        barcodeImage.attr("src", url);
-        barcodeImage.attr("style",  "position: relative;" 
-                                  + "z-index: 2147483647;"// max value of z-index
-                                  + "width: 100%;");
-        barcodeImage.appendTo(barcodeWrapper);
-
-        barcodeData.attr("style", "position: relative;"                                
-                                + "text-align: center;"
-                                + "font-family: Tahoma, Geneva, sans-serif;" 
-                                + "font-size: 25px;"
-                                + "color: black;" 
-                                + "bottom: 30px;"
-                                + "padding: 10px;"
-                                + "z-index: 2147483647;" 
-                                + "background-color: white;");
-        barcodeData.appendTo(barcodeWrapper);
-
-        barcodeWrapper.attr("style", "position: absolute;"
-                                  +  "top: 0;" 
-                                  +  "right: 0;" 
-                                  +  "bottom: 0;"  
-                                  +  "left: 0;" 
-                                  +  "width: 100%;"  
-                                  +  "height: 100%;"  
-                                  +  "min-height: 100%;"
-                                  +  "z-index: 2147483647;" 
-                                  +  "background-color: white;");
-        
-        backgroundMask.attr("style", "position: fixed;"  // div covers 100% of page (not screen) - need it for devices with a really small screens
-                                   + "left: 0px;" 
-                                   + "top: 0px;" 
-                                   + "width: 100%;" 
-                                   + "height: 100%;" 
-                                   + "z-index: 2147483646;" 
-                                   + "background-color: white;");
-
-        barcodeWrapper.appendTo(emulatorBridge.window().document.body);
-        backgroundMask.appendTo(emulatorBridge.window().document.body);
-        emulatorBridge.window().document.addEventListener("backbutton",  barcodeBackButtonFunction, false);    
-//      success(); // ?! under Android VM success function is never called
-    }
-  }
-});
 ripple.define('platform/cordova/2.0.0/bridge/media', function (ripple, exports, module) {
 /*
  *
@@ -40837,9 +40692,18 @@ var _prompt = ripple('ui/plugins/exec-dialog'),
         "Media": ripple('platform/cordova/2.0.0/bridge/media'),
         "Network Status": ripple('platform/cordova/2.0.0/bridge/network'),
         "NetworkStatus": ripple('platform/cordova/2.0.0/bridge/network'),
-            "Notification": ripple('platform/cordova/2.0.0/bridge/notification'),
-            "Vibration": ripple('platform/cordova/2.0.0/bridge/notification'),
-            "BarcodeScanner": ripple('phonegap-plugin/BarcodeScanner')
+        "Notification": ripple('platform/cordova/2.0.0/bridge/notification'),
+        "Vibration": ripple('platform/cordova/2.0.0/bridge/notification'),
+        "SplashScreen": ripple('platform/cordova/3.0.0/bridge/splashscreen'),
+        "InAppBrowser": ripple('platform/cordova/3.0.0/bridge/inappbrowser')
+    },
+    processException = function(success, fail, service, action, args, exception) {
+          console.log("missing exec:" + service + "." + action);
+          console.log(args);
+          console.log(exception);
+          console.log(exception.stack);
+          //TODO: this should really not log the above lines, but they are very nice for development right now
+          _prompt.show(service, action, success, fail);
     };
 
 module.exports = {
@@ -40848,15 +40712,33 @@ module.exports = {
     },
     exec: function (success, fail, service, action, args) {
         try {
-            emulator[service][action](success, fail, args);
-        }
-        catch (e) {
-            console.log("missing exec:" + service + "." + action);
-            console.log(args);
-            console.log(e);
-            console.log(e.stack);
-            //TODO: this should really not log the above lines, but they are very nice for development right now
-            _prompt.show(service, action, success, fail);
+            if (emulator[service]) {
+                emulator[service][action](success, fail, args);
+            } else { // trying to find service in "/ripple/cordova/plugins/" folder 
+                var pluginUrl = window.location.origin + '/ripple/cordova/plugins/' + service + '.js'; // 'service' name = js file name
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", pluginUrl, true);
+                xhr.onreadystatechange = function() {
+                   try {
+                       if (xhr.readyState == 4 && xhr.status == 200) {
+                          var code = xhr.responseText;
+                          emulator[service] = (function(){
+                            var module = {};
+                            eval(code);
+                            return module.exports;
+                          }());
+                          emulator[service][action](success, fail, args);
+                       } else if (xhr.status == 404) {
+                          throw "not implemented";
+                       }
+                   } catch (e) {
+                      processException(success, fail, service, action, args, e);
+                   }
+                };
+                xhr.send();
+            }
+        } catch (e) {
+          processException(success, fail, service, action, args, e);
         }
     }
 };
@@ -41277,8 +41159,8 @@ module.exports = {
             };
 
         bridge.add("PluginManager", ripple('platform/cordova/3.0.0/bridge/PluginManager'));
-        bridge.add("SplashScreen", ripple('platform/cordova/3.0.0/bridge/splashscreen'));
-        bridge.add("InAppBrowser", ripple('platform/cordova/3.0.0/bridge/inappbrowser'));
+        // bridge.add("SplashScreen", ripple('platform/cordova/3.0.0/bridge/splashscreen'));
+        // bridge.add("InAppBrowser", ripple('platform/cordova/3.0.0/bridge/inappbrowser'));
         honeypot.monitor(win, "cordova").andRun(get, set);
     },
 
@@ -52582,8 +52464,8 @@ ripple.define('platform/spec', function (ripple, exports, module) {
 module.exports = {
     "cordova": {
         "1.0.0": ripple('platform/cordova/1.0.0/spec'),
-        "2.0.0": ripple('platform/cordova/2.0.0/spec'),
-        "3.0.0": ripple('platform/cordova/3.0.0/spec')
+        "2.0.0": ripple('platform/cordova/2.0.0/spec')
+        // "3.0.0": ripple('platform/cordova/3.0.0/spec') // !!! UNCOMMENT this later for cordova 3.0.0 support !!!
     },
     "webworks.bb10": { "1.0.0": ripple('platform/webworks.bb10/1.0.0/spec') },
     "webworks.handset": { "2.0.0": ripple('platform/webworks.handset/2.0.0/spec') },
