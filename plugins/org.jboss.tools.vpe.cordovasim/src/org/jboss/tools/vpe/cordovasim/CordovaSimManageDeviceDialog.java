32d5c5c981c4d08653b2db495d7bcfa66acb5973
Konstantin Marmalyukov <kmarmaliykov@exadel.com>
2014-02-11 20:39:01 +0300

    JBIDE-16493 BrowserSim/CordovaSim: Native crashes after switching engine from SWT to JavaFx
    engines switching is now occurs only after restart

--- plugins/org.jboss.tools.vpe.cordovasim/src/org/jboss/tools/vpe/cordovasim/CordovaSimManageDeviceDialog.java
+++ plugins/org.jboss.tools.vpe.cordovasim/src/org/jboss/tools/vpe/cordovasim/CordovaSimManageDeviceDialog.java
@@ -12,10 +12,13 @@ import org.jboss.tools.vpe.cordovasim.model.preferences.CordovaSimSpecificPrefer
  * @author Konstantin Marmalyukov (kmarmaliykov)
  */
 public class CordovaSimManageDeviceDialog extends ManageDevicesDialog {
-
+	/** @see org.jboss.tools.vpe.browsersim.eclipse.callbacks.RestartCallback */
+	private static final String CORDOVASIM_RESTART_COMMAND = "org.jboss.tools.vpe.cordavasim.command.restart:"; //$NON-NLS-1$
+	private static final String CORDOVASIM_RESTART_COMMAND_END = "org.jboss.tools.vpe.cordavasim.command.restart.end"; //$NON-NLS-1$
+	
 	public CordovaSimManageDeviceDialog(Shell parent, int style, CommonPreferences oldCommonPreferences,
-			SpecificPreferences oldSpecificPreferences) {
-		super(parent, style, oldCommonPreferences, oldSpecificPreferences);
+			SpecificPreferences oldSpecificPreferences, String currentUrl) {
+		super(parent, style, oldCommonPreferences, oldSpecificPreferences, currentUrl);
 	}
 
 	@Override
@@ -27,8 +30,18 @@ public class CordovaSimManageDeviceDialog extends ManageDevicesDialog {
 	@Override
 	protected SpecificPreferences create(String selectedDeviceId, boolean useSkins, boolean enableLiveReload, int liveReloadPort, boolean enableTouchEvents, boolean isJavaFx) {
 		return new CordovaSimSpecificPreferences(selectedDeviceId, useSkins, enableLiveReload, liveReloadPort, enableTouchEvents,
-				oldSpecificPreferences.getOrientationAngle(), oldSpecificPreferences.getLocation(),
+				oldSpecificPreferences.getOrientationAngle(), getParent().getLocation(),
 				((CordovaSimSpecificPreferences)oldSpecificPreferences).getCordovaBrowserLocation(),
 				((CordovaSimSpecificPreferences)oldSpecificPreferences).getCordovaBrowserSize(), isJavaFx);
+	}
+	
+	@Override
+	protected void sendRestartCommand() {
+		System.out.println(CORDOVASIM_RESTART_COMMAND);
+		System.out.println(CordovaSimArgs.getRootFolder());
+		System.out.println(CordovaSimArgs.getStartPage());
+		System.out.println("-port"); //$NON-NLS-1$
+		System.out.println(CordovaSimArgs.getPort());
+		System.out.println(CORDOVASIM_RESTART_COMMAND_END);
 	}
 }
