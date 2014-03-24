fec935ddcf7418862744dcc2ff9d12937ef403b3
Gorkem Ercan <gorkem.ercan@gmail.com>
2014-03-24 15:42:26 -0400

    JBIDE-16858-Failure to replace Variables on Config xml files for Cordova plug-ins
    escape the ($) on the variables.

--- cordova/plugins/org.jboss.tools.aerogear.hybrid.core/src/org/jboss/tools/aerogear/hybrid/core/plugin/actions/ActionVariableHelper.java
+++ cordova/plugins/org.jboss.tools.aerogear.hybrid.core/src/org/jboss/tools/aerogear/hybrid/core/plugin/actions/ActionVariableHelper.java
@@ -15,10 +15,10 @@ public final class ActionVariableHelper {
 	public static String replaceVariables(HybridProject project, String xml ) throws CoreException{
 		WidgetModel model = WidgetModel.getModel(project);
 		Widget widget = model.getWidgetForRead();
-		xml = xml.replaceAll("$PACKAGE_NAME", widget.getId());
+		xml = xml.replaceAll("\\$PACKAGE_NAME", widget.getId());
 		List<Preference> preferences = widget.getPreferences();
 		for ( Preference preference : preferences) {
-			String preferenceKey = "$"+preference.getName();
+			String preferenceKey = "\\$"+preference.getName();
 			if(xml.contains(preferenceKey)){
 				xml = xml.replaceAll(preferenceKey, preference.getValue());
 			}
