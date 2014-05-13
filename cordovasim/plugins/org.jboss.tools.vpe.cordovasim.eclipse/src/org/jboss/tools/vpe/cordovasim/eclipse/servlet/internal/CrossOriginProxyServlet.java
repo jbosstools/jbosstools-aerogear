1b53bb167f32856ea85e777c953390ddaf85f02f
Ilya Buziuk <ibuziuk@exadel.com>
2014-05-13 17:13:28 +0300

    JBIDE-16931 CordovaSim: Moving CordovaSim's jetty server to the cordovasim.eclipse plugin

---
+++ cordovasim/plugins/org.jboss.tools.vpe.cordovasim.eclipse/src/org/jboss/tools/vpe/cordovasim/eclipse/servlet/internal/CrossOriginProxyServlet.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2013 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.vpe.cordovasim.eclipse.servlet.internal;
+
+import java.net.MalformedURLException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.eclipse.jetty.client.HttpExchange;
+import org.eclipse.jetty.http.HttpURI;
+import org.eclipse.jetty.servlets.ProxyServlet;
+
+/**
+ * @author Yahor Radtsevich (yradtsevich)
+ */
+public class CrossOriginProxyServlet extends ProxyServlet {
+	private String urlParameterName;
+
+	public CrossOriginProxyServlet(String urlParameterName) {
+		this.urlParameterName = urlParameterName;
+	}
+	
+	@Override
+	protected HttpURI proxyHttpURI(HttpServletRequest request, String uri)
+			throws MalformedURLException {;
+		String proxiedUrl = request.getParameter(urlParameterName);
+		if (proxiedUrl != null) {
+			return new HttpURI(proxiedUrl);				
+		} else {
+			return null;
+		}
+	}
+	
+	@Override
+	protected void customizeExchange(HttpExchange exchange,
+			HttpServletRequest request) {
+		HttpURI url = new HttpURI(exchange.getRequestURI());
+		exchange.setRequestHeader("Host", url.getHost()); //$NON-NLS-1$
+	}
+}
