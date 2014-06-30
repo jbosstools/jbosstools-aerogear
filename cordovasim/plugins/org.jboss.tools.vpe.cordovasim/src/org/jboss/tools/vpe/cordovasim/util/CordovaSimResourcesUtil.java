49131447e27916dfce98e8e5dc14b1593476f82a
Ilya Buziuk <ibuziuk@exadel.com>
2014-06-30 18:41:55 +0300

    JBIDE-17718 CordovaSim: Fixing CordovaSim's preferences directory

--- cordovasim/plugins/org.jboss.tools.vpe.cordovasim/src/org/jboss/tools/vpe/cordovasim/util/CordovaSimResourcesUtil.java
+++ cordovasim/plugins/org.jboss.tools.vpe.cordovasim/src/org/jboss/tools/vpe/cordovasim/util/CordovaSimResourcesUtil.java
@@ -10,9 +10,13 @@
  ******************************************************************************/
 package org.jboss.tools.vpe.cordovasim.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
-import org.jboss.tools.vpe.browsersim.util.ResourcesUtil;
+import org.jboss.tools.vpe.cordovasim.CordovaSimLogger;
+import org.jboss.tools.vpe.cordovasim.CustomBrowserSim;
 
 /**
  * @author Ilya Buziuk (ibuziuk)
@@ -21,10 +25,34 @@ public class CordovaSimResourcesUtil {
 	private static final String RESOURCES_ROOT_FOLDER = "/org/jboss/tools/vpe/cordovasim/resources/";  //$NON-NLS-1$
 
 	public static InputStream getResourceAsStream(String name) {
-		return ResourcesUtil.getResourceAsStream(RESOURCES_ROOT_FOLDER, name);
+		if (name.startsWith("/")) { //$NON-NLS-1$
+			return CustomBrowserSim.class.getResourceAsStream(name);
+		} else {
+			return CustomBrowserSim.class.getResourceAsStream(RESOURCES_ROOT_FOLDER + name);
+		}
 	}
 	
 	public static String getResourceAsString(String name) {
-		return ResourcesUtil.getResourceAsString(RESOURCES_ROOT_FOLDER, name);
+		InputStream input = getResourceAsStream(name);
+		InputStreamReader reader = new InputStreamReader(input);
+		BufferedReader bufferedReader = new BufferedReader(reader);
+
+		StringBuilder stringBuilder = new StringBuilder();
+		String read;
+		try {
+			try {
+				while ((read = bufferedReader.readLine()) != null) {
+					stringBuilder.append(read);
+					stringBuilder.append('\n');
+				}
+			} finally {
+				bufferedReader.close();
+			}
+		} catch (IOException e) {
+			CordovaSimLogger.logError(e.getMessage(), e);
+		}
+
+		return stringBuilder.toString();
 	}
+		
 }
