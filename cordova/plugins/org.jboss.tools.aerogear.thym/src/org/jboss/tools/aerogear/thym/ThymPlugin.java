1e75720ffe479bc7ce26fa0112d51ae0cab87a1e
Gorkem Ercan <gorkem.ercan@gmail.com>
2014-08-12 17:33:28 +0300

    JBIDE-17999 - migration of aerogear nature ids to thym
    Adds a resource listener which replaces the legacy aerogear nature id
    with the new Thym nature id

--- cordova/plugins/org.jboss.tools.aerogear.thym/src/org/jboss/tools/aerogear/thym/ThymPlugin.java
+++ cordova/plugins/org.jboss.tools.aerogear.thym/src/org/jboss/tools/aerogear/thym/ThymPlugin.java
@@ -1,6 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.aerogear.thym;
 
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.jboss.tools.aerogear.thym.internal.convert.NatureConvertListener;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -10,9 +26,11 @@ public class ThymPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.jboss.tools.aerogear.thym"; //$NON-NLS-1$
-
 	// The shared instance
 	private static ThymPlugin plugin;
+	private static ILog logger;
+	
+	
 	
 	/**
 	 * The constructor
@@ -27,6 +45,7 @@ public class ThymPlugin extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		logger = Platform.getLog(this.getBundle());
 	}
 
 	/*
@@ -35,7 +54,11 @@ public class ThymPlugin extends AbstractUIPlugin {
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
+		if(NatureConvertListener.getInstance() != null){
+			ResourcesPlugin.getWorkspace().removeResourceChangeListener(NatureConvertListener.getInstance());
+		}
 		super.stop(context);
+		
 	}
 
 	/**
@@ -46,5 +69,14 @@ public class ThymPlugin extends AbstractUIPlugin {
 	public static ThymPlugin getDefault() {
 		return plugin;
 	}
-
+	/**
+	 * Log to bundle's log.
+	 * 
+	 * @param status
+	 * @param message
+	 * @param throwable
+	 */
+	public static void log(int status, String message, Throwable throwable ){
+		logger.log(new Status(status, message, PLUGIN_ID,throwable));
+	}
 }
---
+++ cordova/plugins/org.jboss.tools.aerogear.thym/src/org/jboss/tools/aerogear/thym/internal/convert/NatureConvertListener.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.aerogear.thym.internal.convert;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.thym.core.natures.HybridAppNature;
+import org.eclipse.ui.IStartup;
+import org.jboss.tools.aerogear.thym.ThymPlugin;
+
+public class NatureConvertListener implements IResourceChangeListener, IStartup {
+	
+	private static final String LEGACY_NATURE_ID = "org.jboss.tools.aerogear.hybrid.core.HybridAppNature";
+	private static NatureConvertListener instance;
+
+	@Override
+	public void resourceChanged(IResourceChangeEvent event) {
+		IResourceDelta[] projectDeltas = event.getDelta().getAffectedChildren();
+		for (IResourceDelta delta : projectDeltas) {
+			if (delta.getResource().getType() == IResource.PROJECT) {
+				final IProject project = delta.getResource().getProject();
+				try {
+					if (project.hasNature(LEGACY_NATURE_ID)
+							&& !project.hasNature(HybridAppNature.NATURE_ID)) {
+						WorkspaceJob job = new WorkspaceJob(
+								"Convert to Thym nature") {
+
+							@Override
+							public IStatus runInWorkspace(
+									IProgressMonitor monitor)
+									throws CoreException {
+								IProjectDescription desc = project
+										.getDescription();
+								String[] natures = desc.getNatureIds();
+								for (int i = 0; i < natures.length; i++) {
+									if (natures[i].equals(LEGACY_NATURE_ID)) {
+										natures[i] = HybridAppNature.NATURE_ID;
+									}
+								}
+								desc.setNatureIds(natures);
+								project.setDescription(desc, monitor);
+								return Status.OK_STATUS;
+							}
+						};
+						job.schedule();
+					}
+				} catch (CoreException e) {
+					ThymPlugin.log(IStatus.WARNING,
+							"error while reading natures", e);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void earlyStartup() {
+		this.instance = new NatureConvertListener();
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(instance,IResourceChangeEvent.POST_CHANGE);
+	}
+	
+	public static final NatureConvertListener getInstance(){
+		return NatureConvertListener.instance;
+	}
+
+}
