5d87f24d82f2638c8d5e0d2ca9ebf93972e8c726
Gorkem Ercan <gorkem.ercan@gmail.com>
2015-01-21 17:56:38 -0500

    Fix for the broken destination history

--- feedhenry/plugins/org.jboss.tools.feedhenry.ui/src/org/jboss/tools/feedhenry/ui/FHPlugin.java
+++ feedhenry/plugins/org.jboss.tools.feedhenry.ui/src/org/jboss/tools/feedhenry/ui/FHPlugin.java
@@ -59,8 +59,8 @@ public final class FHPlugin extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
 		super.stop(context);
+		plugin = null;
 	}
 
 	/**
