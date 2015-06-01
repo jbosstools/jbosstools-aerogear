c27f97c5f44fd1ce1dcb87bad21587c353824a6c
Denis Golovin <dgolovin@gmail.com>
2015-06-01 10:34:44 -0700

    Fix build errors with new TP

--- cordovasim/tests/org.jboss.tools.cordovasim.eclipse.test/src/org/jboss/tools/corodvasim/eclipse/test/JettyBundlesTest.java
+++ cordovasim/tests/org.jboss.tools.cordovasim.eclipse.test/src/org/jboss/tools/corodvasim/eclipse/test/JettyBundlesTest.java
@@ -30,14 +30,14 @@ import org.osgi.framework.Version;
 @RunWith(JUnit4.class)
 public class JettyBundlesTest {
 	// Current version in TP is 9.2.9
-	private static final String VERSION = "9.2.9.v20150224"; //$NON-NLS-1$
+	private static final String VERSION = "9.2.10.v20150310"; //$NON-NLS-1$
 
 	@Test
 	public void testJettyVersionMatches() {
 		List<String> bundles = getJettyBundles();
 		for (String bundleName : bundles) {
 			Bundle bundle = Platform.getBundle(bundleName);
-			assertNotNull(bundle);
+			assertNotNull(bundleName + " not found", bundle);
 			Version version = bundle.getVersion();
 			assertNotNull(version);
 			assertEquals(VERSION, version.toString());
