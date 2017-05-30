a59d3f615367dccffd8f86607db7ecac52314744
Jeff MAURY <jeffmaury.github@jeffmaury.com>
2017-05-30 13:53:20 +0200

    JBIDE-24515 - org.jboss.tools.browsersim.ui requires jetty 9.4.0, but 9.4.5 is in Oxygen.0.M7 (#168)
    * JBIDE-24515 extend range of supported versions of o.e.jetty.* to include up to 9.4.x as 9.4.5 is in Oxygen.0.M7
    
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    * JBIDE-24515 - org.jboss.tools.browsersim.ui requires jetty 9.4.0, but 9.4.5 is in Oxygen.0.M7
    
    - Update jetty dependencies
    - Fix failing tests
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- cordovasim/tests/org.jboss.tools.cordovasim.eclipse.test/src/org/jboss/tools/cordovasim/eclipse/test/JettyBundlesTest.java
+++ cordovasim/tests/org.jboss.tools.cordovasim.eclipse.test/src/org/jboss/tools/cordovasim/eclipse/test/JettyBundlesTest.java
@@ -29,8 +29,8 @@ import org.osgi.framework.Version;
  */
 @RunWith(JUnit4.class)
 public class JettyBundlesTest {
-	// Current version in TP is 9.4.0.v20161208
-	private static final String VERSION = "9.4.0.v20161208"; //$NON-NLS-1$
+	// Current version in TP is 9.4.5.v20170502
+	private static final String VERSION = "9.4.5.v20170502"; //$NON-NLS-1$
 
 	@Test
 	public void testJettyVersionMatches() {
